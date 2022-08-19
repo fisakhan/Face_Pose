2e04cdfc20e8c1f94db0ff7bc4dbcbaa97236926
khanfisa <asifspecial@gmail.com>
2022-08-19 17:05:24 +0200

    verify that coordinates are within the camera frame

--- pose_detection_retinaface.py
+++ pose_detection_retinaface.py
@@ -1,34 +1,29 @@
 """
-October 03, 2020
 Author: Asif Khan
 """
 import tensorflow as tf
 import numpy as np
 import cv2
 
-# MTCNN face detector
-#from mtcnn.mtcnn import MTCNN
-#detector = MTCNN()
-
-# RetinaFace face detector
-detector_model = tf.saved_model.load('./tf_retinaface_mbv2/')
+# load retinaFace face detector
+detector_model = tf.saved_model.load('./models/tf_retinaface_mbv2/')
 
 def one_face(frame, bbs, pointss):
     """
     Parameters
     ----------
-    frame : TYPE
+    frame : uint8
         RGB image (numpy array).
-    bbs : TYPE - Array of flaot64, Size = (N, 5)
+    bbs : float64, Size = (N, 4)
         coordinates of bounding boxes for all detected faces.
-    pointss : TYPE - Array of flaot32, Size = (N, 10)
+    pointss : flaot32, Size = (N, 10)
         coordinates of landmarks for all detected faces.
 
     Returns
     -------
-    bb : TYPE - Array of float 64, Size = (5,)
+    bb : float64, Size = (5,)
         coordinates of bounding box for the selected face.
-    points : TYPE
+    points : float32
         coordinates of five landmarks for the selected face.
 
     """
@@ -40,16 +35,42 @@ def one_face(frame, bbs, pointss):
     bb = bbs[index]
     points = pointss[:,index]
     return bb, points
+
+def are_coordinates_in_frame(frame, box, pts):
+    """
+    Parameters
+    ----------
+    frame : uint8
+        RGB image (numpy array).
+    bbs : float64
+        coordinates of bounding box.
+    points : flaot32
+        coordinates of landmarks.
+
+    Returns
+    -------
+    boolean
+    """
+    
+    height, width = frame.shape[:2]
+    
+    if np.any(box <= 0) or np.any(box >= height) or np.any(box >= width):
+        return False
+    if np.any(pts <= 0) or np.any(pts >= height) or np.any(pts >= width):
+        return False
+    
+    return True
+    
             
 def draw_landmarks(frame, bb, points):
     '''
     Parameters
     ----------
-    frame : TYPE
+    frame : uint8
         RGB image
-    bb : TYPE - Array of float64, Size = (5,)
+    bb : float64, Size = (5,)
         coordinates of bounding box for the selected face.
-    points : TYPE - Array of float32, Size = (10,)
+    points : float32, Size = (10,)
         coordinates of landmarks for the selected faces.
 
     Returns
@@ -80,9 +101,20 @@ def draw_landmarks(frame, bb, points):
     if w2h_ratio < 0.7 or w2h_ratio > 0.9:
         cv2.putText(frame, "Face: long and narrow", (10, 160), font, font_size, blue, 1)
 
-def find_smile(pts):
-    dx_eyes = pts[1] - pts[0]# between pupils
-    dx_mout = pts[4] - pts[3]# between mouth corners
+def find_smile(points):
+    """
+    Parameters
+    ----------
+    points : flaot32
+        coordinates of landmarks.
+
+    Returns
+    -------
+    smile_ratio : float32
+        a value that determines if the face is smiling.
+    """
+    dx_eyes = points[1] - points[0]# pixels between pupils
+    dx_mout = points[4] - points[3]# pixles between mouth corners
     smile_ratio = dx_mout/dx_eyes    
     return smile_ratio
 
@@ -90,12 +122,13 @@ def find_roll(points):
     """
     Parameters
     ----------
-    points : TYPE - Array of float32, Size = (10,)
-        coordinates of landmarks for the selected faces.
+    points : float32
+        coordinates of landmarks.
+
     Returns
     -------
-    TYPE
-        roll of face.
+    flaot32
+        an indication of roll.
 
     """
     return points[6] - points[5]
@@ -104,12 +137,12 @@ def find_yaw(points):
     """
     Parameters
     ----------
-    points : TYPE - Array of float32, Size = (10,)
-        coordinates of landmarks for the selected faces.
+    points : float32, Size = (10,)
+        coordinates of landmarks.
     Returns
     -------
-    TYPE
-        yaw of face.
+    float32
+        an indication of yaw.
 
     """
     le2n = points[2] - points[0]
@@ -120,11 +153,12 @@ def find_pitch(points):
     """
     Parameters
     ----------
-    points : TYPE - Array of float32, Size = (10,)
-        coordinates of landmarks for the selected faces.
+    points : float32, Size = (10,)
+        coordinates of landmarks.
     Returns
     -------
-    Pitch
+    float32
+        an indication of pitch.
     """
     eye_y = (points[5] + points[6]) / 2
     mou_y = (points[8] + points[9]) / 2
@@ -136,23 +170,18 @@ def find_pose(points):
     """
     Parameters
     ----------
-    points : TYPE - Array of float32, Size = (10,)
+    points : float32, Size = (10,)
         coordinates of landmarks for the selected faces.
     Returns
     -------
-    Angle
-    Yaw
-    Pitch
-    TYPE
-        pitch of face.
-
+    float32, float32, float32
     """
     LMx = points[0:5]# horizontal coordinates of landmarks
     LMy = points[5:10]# vertical coordinates of landmarks
     
     dPx_eyes = max((LMx[1] - LMx[0]), 1)
     dPy_eyes = (LMy[1] - LMy[0])
-    angle = np.arctan(dPy_eyes / dPx_eyes) # angle for rotation based on slope eyes
+    angle = np.arctan(dPy_eyes / dPx_eyes) # angle for rotation based on slope
     
     alpha = np.cos(angle)
     beta = np.sin(angle)
@@ -175,24 +204,17 @@ def find_pose(points):
 
     return angle * 180 / np.pi, Xfrontal, Yfrontal
 
-def face_detector(image, image_shape_max=640, score_min=None, pixel_min=None, pixel_max=None, Ain_min=None):
+def detect_faces(image, image_shape_max=640):
     '''
-    Performs face detection using retinaface method with speed boost and initial quality checks based on whole image size
+    Performs face detection using retinaface method with speed boost and 
+    initial quality checks based on whole image size
     
     Parameters
     ----------
     image : uint8
         image for face detection.
     image_shape_max : int, optional
-        maximum size (in pixels) of image. The default is None.
-    score_min : float, optional
-        minimum detection score (0 to 1). The default is None.
-    pixel_min : int, optional
-        mininmum face size based on heigth of bounding box. The default is None.
-    pixel_max : int, optional
-        maximum face size based on heigth of bounding box. The default is None.
-    Ain_min : float, optional
-        minimum area of face in bounding box. The default is None.
+        maximum size (in pixels) of image.
 
     Returns
     -------
@@ -202,9 +224,6 @@ def face_detector(image, image_shape_max=640, score_min=None, pixel_min=None, pi
         bounding boxes.
     flaot array
         detection scores.
-    float array
-        face area in bounding box.
-
     '''
 
     image_shape = image.shape[:2]
@@ -216,64 +235,21 @@ def face_detector(image, image_shape_max=640, score_min=None, pixel_min=None, pi
         scale_factor = 1
         
     if scale_factor > 1:        
-        scaled_image = cv2.resize(image, (0, 0), fx=1/scale_factor, fy=1/scale_factor)
+        scaled_image = cv2.resize(image, (0, 0), fx = 1 / scale_factor, 
+                                  fy = 1 / scale_factor)
         bbs_all, points_all = retinaface(scaled_image)
-        bbs_all[:,:4]*=scale_factor
-        points_all*=scale_factor
+        bbs_all[:,:4] *= scale_factor
+        points_all *= scale_factor
     else:
-        bbs_all, points_all = retinaface(image)
+        bbs_all, points_all = retinaface(image)              
     
-    bbs=bbs_all.copy()
-    points=points_all.copy()
+    scores = bbs_all[:,-1]
+    bbs = bbs_all[:, :4]
     
-    # check detection score
-    if score_min:
-        mask=np.array(bbs[:,4]>score_min)
-        bbs=bbs[mask]
-        points=points[mask]
-        if len(bbs)==0:
-            return [],[],[],[]           
-
-    # check pixel height
-    if pixel_min: 
-        pixel=bbs[:,3]-bbs[:,1]
-        mask=np.array(pixel>pixel_min)
-        bbs=bbs[mask]
-        points=points[mask]
-        if len(bbs)==0:
-            return [],[],[],[]           
-
-    if pixel_max: 
-        pixel=bbs[:,3]-bbs[:,1]
-        mask=np.array(pixel<pixel_max)
-        bbs=bbs[mask]
-        points=points[mask]
-        if len(bbs)==0:
-            return [],[],[],[]           
-
-    # check face area in bounding box
-    Ains = []
-    for bb in bbs:
-        Win=min(image_shape[1],bb[2])-max(0,bb[0])
-        Hin=min(image_shape[0],bb[3])-max(0,bb[1])
-        Abb=(bb[2]-bb[0])*(bb[3]-bb[1])
-        Ains.append(Win*Hin/Abb*100 if Abb!=0 else 0)
-    Ains = np.array(Ains)
-
-    if Ain_min:
-        mask=np.array(Ains>=Ain_min)
-        bbs=bbs[mask]
-        points=points[mask]
-        Ains=Ains[mask]
-        if len(bbs)==0:
-            return [],[],[],[]           
-    
-    scores = bbs[:,-1]
-    bbs = bbs[:, :4]
-    
-    return points, bbs, scores, Ains
+    return points_all, bbs, scores
 
 def retinaface(image):
+    """ retinaface face detector"""
 
     height = image.shape[0]
     width = image.shape[1]
@@ -300,7 +276,7 @@ def retinaface(image):
     return bbs, lms
 
 def pad_input_image(img, max_steps=32):
-    """pad image to suitable shape"""
+    """pad image to suitable shape - required for retinaface"""
     img_h, img_w, _ = img.shape
 
     img_pad_h = 0
@@ -330,14 +306,12 @@ def recover_pad_output(outputs, pad_params):
 #============================================================================
 # FONT SETTING (font style, size and color)
 font = cv2.FONT_HERSHEY_COMPLEX # Text in video
-font_size = 0.4
 blue = (0, 0, 255)
 green = (0,128,0)
 red = (255, 0, 0)
 
 # RECORDING SETTING (Recordings on/off)
-image_save = False# save face image
-video_save = True# save video
+video_save = False# do you want to record video?
 fps = 10.
 video_format = cv2.VideoWriter_fourcc('M','J','P','G')
 if video_save:
@@ -366,49 +340,61 @@ while (True):
     
     # face detection
     try:
-        pointss_all, bbs_all, scores_all, area_in = face_detector(image_rgb, image_shape_max=640, score_min=0.95,
-                                                      pixel_min=20, pixel_max=1000, Ain_min=90)
+        landmarks, bboxes, scores = detect_faces(image_rgb,image_shape_max=640)
     except:
         print("Error: face detector error.")
         break
     
     # if at least one face is detected
-    if len(bbs_all) > 0:
+    if len(bboxes) > 0:
         
-        bbs_all = np.insert(bbs_all,bbs_all.shape[1], scores_all, axis=1)
-        pointss_all = np.transpose(pointss_all)
-        bbs = bbs_all.copy()
-        lmarks = pointss_all.copy()
+        #bboxes = np.insert(bboxes,bboxes.shape[1], scores, axis=1)
+        lmarks = np.transpose(landmarks)
+        bbs = bboxes.copy()
     
         # process only one face (center ?) if multiple faces detected  
         bb, lmarks_5 = one_face(image_rgb, bbs, lmarks)
-        draw_landmarks(image_rgb, bb, lmarks_5)# draw landmarks and bbox   
+        if are_coordinates_in_frame(image_rgb, bb, lmarks_5):
+            
+            draw_landmarks(image_rgb, bb, lmarks_5)# draw landmarks and bbox   
+            
+            cv2.putText(image_rgb, "Face Pose", (10, 40), font, 0.8, blue, 2) 
+            cv2.putText(image_rgb, "Method 1", (10, 60), font, 0.4, blue, 2) 
+            cv2.putText(image_rgb, "Roll: {0:.2f} (-50 to +50)".format(
+                find_roll(lmarks_5)), (10, 80), font, 0.4, blue, 1)  
+            cv2.putText(image_rgb, "Yaw: {0:.2f} (-100 to +100)".format(
+                find_yaw(lmarks_5)), (10, 100), font, 0.4, blue, 1)
+            cv2.putText(image_rgb, "Pitch: {0:.2f} (0 to 4)".format(
+                find_pitch(lmarks_5)), (10, 120), font, 0.4, blue, 1)
+            
+            angle, Xfrontal, Yfrontal = find_pose(lmarks_5)
+            cv2.putText(image_rgb, "Method 2", (10, 180), font, 0.4, blue, 2)
+            cv2.putText(image_rgb, "Roll: {0:.2f} degrees".format(angle), 
+                        (10,200), font, 0.4, blue, 1)
+            cv2.putText(image_rgb, "Yaw: {0:.2f} degrees".format(Xfrontal), 
+                        (10,220), font, 0.4, blue, 1)
+            cv2.putText(image_rgb, "Pitch: {0:.2f} degrees".format(Yfrontal), 
+                        (10,240), font, 0.4, blue, 1)
+            
+            # smile detection
+            smile_ratio = find_smile(lmarks_5) 
+            if smile_ratio > 0.85:
+                cv2.putText(image_rgb, "Smile: Yes", (10,280), font, 0.4, 
+                            green, 1)
+            else:
+                cv2.putText(image_rgb, "Smile: No", (10,280), font, 0.4, 
+                            (0,0,0), 1)  
         
-        cv2.putText(image_rgb, "Face Pose", (10, 40), font, 0.8, blue, 2) 
-        cv2.putText(image_rgb, "Method 1", (10, 60), font, font_size, blue, 2) 
-        cv2.putText(image_rgb, "Roll: {0:.2f} (-50 to +50)".format(find_roll(lmarks_5)), (10, 80), font, font_size, blue, 1)  
-        cv2.putText(image_rgb, "Yaw: {0:.2f} (-100 to +100)".format(find_yaw(lmarks_5)), (10, 100), font, font_size, blue, 1)
-        cv2.putText(image_rgb, "Pitch: {0:.2f} (0 to 4)".format(find_pitch(lmarks_5)), (10, 120), font, font_size, blue, 1)
-        
-        angle, Xfrontal, Yfrontal = find_pose(lmarks_5)
-        cv2.putText(image_rgb, "Method 2", (10, 180), font, font_size, blue, 2)
-        cv2.putText(image_rgb, "Roll: {0:.2f} degrees".format(angle), (10,200), font, font_size, blue, 1)
-        cv2.putText(image_rgb, "Yaw: {0:.2f} degrees".format(Xfrontal), (10,220), font, font_size, blue, 1)
-        cv2.putText(image_rgb, "Pitch: {0:.2f} degrees".format(Yfrontal), (10,240), font, font_size, blue, 1)
-        
-        # smile detection
-        smile_ratio = find_smile(lmarks_5) 
-        if smile_ratio > 0.85:
-            cv2.putText(image_rgb, "Smile: Yes", (10,280), font, font_size, green, 1)
         else:
-            cv2.putText(image_rgb, "Smile: No", (10,280), font, font_size, (0,0,0), 1)
+            cv2.putText(image_rgb, 'move face to center', (10, 20), font, 0.4, 
+                        blue, 2)
             
     else:
-        cv2.putText(image_rgb, 'no face detected', (10, 20), font, font_size, blue, 2)
+        cv2.putText(image_rgb, 'no face detected', (10,20), font, 0.4, blue, 2)
     
-    cv2.putText(image_rgb, "Press Q to quit.", (10, 460), font, font_size, blue, 1)
+    cv2.putText(image_rgb, "Press Q to quit.", (10, 460), font, 0.4, blue, 1)
     image_bgr = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)            
-    cv2.imshow('Pose Detection - Retina Face', image_bgr)    
+    cv2.imshow('Pose Detection - RetinaFace', image_bgr)    
     
     # write original frames to disk
     if video_save:
